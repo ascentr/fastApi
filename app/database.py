b1432faef5afa2d6bcb61f51be87ad37ea3f0205
amjad saleem <ascentrick@gmail.com>
2021-12-13 23:30:28 +0000

    added sslmode='required' to database.py

--- app/database.py
+++ app/database.py
@@ -15,7 +15,8 @@ from .config import settings
 
 SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+#engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, sslmode='require' )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
