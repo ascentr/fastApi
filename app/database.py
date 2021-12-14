8729ac4458f1ba751af9e71ebcdf12e0522fa121
amjad saleem <ascentrick@gmail.com>
2021-12-14 14:35:02 +0000

    use settings.database_URL

--- app/database.py
+++ app/database.py
@@ -13,9 +13,11 @@ from .config import settings
 
 #SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:amjad@localhost/fastapi'
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+#SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL , sslmode="require")
+SQLALCHEMY_DATABASE_URL = f'{settings.database_URL}, sslMode="require"'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
