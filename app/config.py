153d5b9f1e02f0f4e48f4ed46b6f7737df197517
amjad saleem <ascentrick@gmail.com>
2021-12-14 22:51:06 +0000

    set up database_url

--- app/config.py
+++ app/config.py
@@ -11,6 +11,9 @@ class Settings(BaseSettings):
     algorithm : str
     access_token_expires_minutes: int
 
+    database_url = f'postgresql://{database_username}:{database_password}@{database_hostname}:{database_port}/{database_name}'
+
+
     class Config:
         env_file = ".env"
 
--- app/database.py
+++ app/database.py
@@ -13,9 +13,9 @@ from .config import settings
 
 #SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:amjad@localhost/fastapi'
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+#SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
-#SQLALCHEMY_DATABASE_URL = f'{settings.database_URL}, sslMode="require"'
+SQLALCHEMY_DATABASE_URL = f'{settings.database_url}, sslMode="require"'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
