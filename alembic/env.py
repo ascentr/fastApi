3392a719135d8bdfa4db676e0cc7d6e3544a4495
amjad saleem <ascentrick@gmail.com>
2021-12-14 14:05:03 +0000

    removed sslmode='require in env.py

--- alembic/env.py
+++ alembic/env.py
@@ -44,7 +44,7 @@ def run_migrations_offline():
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = config.get_main_option("sqlalchemy.url" , sslmode='require')
     context.configure(
         url=url,
         target_metadata=target_metadata,
